628ad6a0d3410789a9beb52fc7060ee04c04bd4f
Motaz Khalifa <36167041+motazkhalifa98@users.noreply.github.com>
2021-05-13 18:03:29 -0400

    Commented and cleaned the code

--- Program.cs
+++ Program.cs
@@ -18,16 +18,15 @@ namespace HERO_XInput_Gampad_Example
         static CTRE.Phoenix.PneumaticControlModule _pcm = new CTRE.Phoenix.PneumaticControlModule(0); //creating a PCM object
         SafeOutputPort digitalOut1 = new SafeOutputPort(CTRE.HERO.IO.Port1.Pin4, false);  //PIN 4 > IN2
         SafeOutputPort digitalOut2 = new SafeOutputPort(CTRE.HERO.IO.Port1.Pin5, false);   //PIN 5 > IN1
-        InputPort inputDead = new InputPort(CTRE.HERO.IO.Port6.Pin4, false, Port.ResistorMode.PullDown);
-        InputPort inputPressure = new InputPort(CTRE.HERO.IO.Port6.Pin5, false, Port.ResistorMode.Disabled);
+        InputPort inputDead = new InputPort(CTRE.HERO.IO.Port6.Pin4, false, Port.ResistorMode.PullDown); //Deadman switch, Port6 Pin4
+        InputPort inputPressure = new InputPort(CTRE.HERO.IO.Port6.Pin5, false, Port.ResistorMode.Disabled); //Input Pressure from Pi Port 26 connected to Port6 Pin5
         TalonSRX tal1 = new TalonSRX(1); //first Talon, ID = 1
         TalonSRX tal2 = new TalonSRX(2);//second Talon, ID = 2
-        long kTimeoutPeriod;
-        long solenoidPeriod;
+        long solenoidPeriod; //Time to auto-shutdown solenoid valve after shooting/opening it.
 
         public void RunForever()
         {
-            _pcm.SetSolenoidOutput(1, false);
+            _pcm.SetSolenoidOutput(1, false); //Initialize the compressor to be turned off
             Boolean SolenoidTimer = false;
             tal1.ConfigFactoryDefault();
             tal2.ConfigFactoryDefault();
@@ -43,7 +42,7 @@ namespace HERO_XInput_Gampad_Example
                 }
                 else
                 {
-                    Debug.Print("Not connected: " + i);
+                    Debug.Print("Not connected: " + i); //The controller is not connected
                 }
                 i++;
 
@@ -74,21 +73,21 @@ namespace HERO_XInput_Gampad_Example
                 if (Pressure_Switch)
                 {
                     pressure = "Above threshold";
-                    _pcm.SetSolenoidOutput(1, false);
+                    _pcm.SetSolenoidOutput(1, false);	//Pressure is above threshold, turn off solenoid
                 }
-                Debug.Print("Pressure Value: " + pressure);
+                Debug.Print("Pressure Value: " + pressure); //Print pressure value
 
                 //Compressor
                 Boolean StartCompressor = _gamepad.GetButton(10); //"START"-Button
                 Boolean StopCompressor = _gamepad.GetButton(9); //"BACK"-Button
-                if (StartCompressor && (!Pressure_Switch))
+                if (StartCompressor && (!Pressure_Switch)) //If pressure is below threshold and "START" is pressed
                 {
-                    _pcm.SetSolenoidOutput(1, true);
+                    _pcm.SetSolenoidOutput(1, true); //Start compressor
                     Debug.Print("StartCompressor");
                 }
-                if (StopCompressor)
+                if (StopCompressor) //If "BACK" is pressed
                 {
-					_pcm.SetSolenoidOutput(1, false);
+		    _pcm.SetSolenoidOutput(1, false); //Stop compressor
 					
                     Debug.Print("StopCompressor");
                 }
@@ -100,33 +99,33 @@ namespace HERO_XInput_Gampad_Example
                 Boolean FIRE = _gamepad.GetButton(4); //Y-Button
                 Boolean Deadman_Switch = inputDead.Read();
                 String dead = "OFF";
-                if (Deadman_Switch)
+                if (Deadman_Switch) //Current state of deadman switch
                 {
                     dead = "ON";
                 }
                 //Debug.Print("Button Value: " + dead);
-                if (FIRE && (!Deadman_Switch))
+                if (FIRE && (!Deadman_Switch)) //If Y is pressed and deadman switch is off
                 {
-                    _pcm.SetSolenoidOutput(0, true);
-                    solenoidPeriod = (500 * TimeSpan.TicksPerMillisecond) + DateTime.Now.Ticks;
+                    _pcm.SetSolenoidOutput(0, true); //Open Solenoid/Fire
+                    solenoidPeriod = (500 * TimeSpan.TicksPerMillisecond) + DateTime.Now.Ticks; //Start timer for half a second
                     Debug.Print("FIRE");
                     SolenoidTimer = true;
                 }
                 if (SolenoidTimer)
                 {
                     long nowSolenoid = DateTime.Now.Ticks;
-                    if (nowSolenoid > solenoidPeriod)
+                    if (nowSolenoid > solenoidPeriod) //If half a second has passed
                     {
-                        _pcm.SetSolenoidOutput(0, false);
+                        _pcm.SetSolenoidOutput(0, false); //Close the solenoid
                         SolenoidTimer = false;
                         Debug.Print("Close Solenoid");
                     }
                 }
 
-                Boolean LeftForward = _gamepad.GetButton(5);
-                Boolean LeftBackward = _gamepad.GetButton(7);
-                Boolean RightForward = _gamepad.GetButton(6);
-                Boolean RightBackward = _gamepad.GetButton(8);
+                Boolean LeftForward = _gamepad.GetButton(5); //LB
+                Boolean LeftBackward = _gamepad.GetButton(7); //LT
+                Boolean RightForward = _gamepad.GetButton(6); //RB
+                Boolean RightBackward = _gamepad.GetButton(8); //RT
 
                 float LeftY = 0;
                 float RightY = 0;
@@ -148,13 +147,10 @@ namespace HERO_XInput_Gampad_Example
                 {
                     RightY = (float)(-0.5);
                 }
-                //     float LeftY = _gamepad.GetAxis(1); //Left Joystick
-                //    float RightY = _gamepad.GetAxis(3); //Right Joystick
 
-                tal1.Set(ControlMode.PercentOutput, LeftY * -1); //moving tal1 with left joystick
-                                                                 //   Debug.Print("Left Axis: " + LeftY);
-                                                                 //  Debug.Print("Right Axis: " + RightY);
-                tal2.Set(ControlMode.PercentOutput, RightY); //moving tal2 with right joystick
+
+                tal1.Set(ControlMode.PercentOutput, LeftY * -1); //moving tal1 with LT & LB
+                tal2.Set(ControlMode.PercentOutput, RightY); //moving tal2 with RT & RB
 
                 System.Threading.Thread.Sleep(10);
 
